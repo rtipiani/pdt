ef9b4dca17d40b7fbf92331ef40b3df4016c3607
Rubén Tipiani <38152657+rtipiani@users.noreply.github.com>
2025-08-04 16:59:26 -0500

    SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: vercel({}),
+    site: 'https://pdtperuviantravel.vercel.app',
     i18n: {
         defaultLocale: 'es',
         locales: ['es', 'en'],
